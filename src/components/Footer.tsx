7e126729f2fb26b549b01a8874ed415b3c6d0d44
Cindy <cindy951357@gmail.com>
2024-07-05 14:48:41 +0800

    horizontal line animation

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="flex w-full bg-rose-500 h-20 bottom-0 fixed justify-center items-center text-white">
+            <div className="line-container">
+            <div className="line"></div>
+            <div className="glowing-dot"></div>
+            </div>
+            © 2024 Cindy. All rights reserved.
+        </footer>
+    )
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode }  from 'react';
 import Menu from './Menu';
 import { useTranslation } from 'next-i18next';
+import Footer from './Footer';
 
 interface LayoutProps {
     children: ReactNode;
@@ -15,9 +16,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="content flex justify-center">
         {children}
       </div>
-      <footer className="flex w-full bg-rose-500 h-20 bottom-0 fixed justify-center items-center text-white">
-        {t('footerText')}
-      </footer>
+      <Footer/>
     </div>
   );
 };
