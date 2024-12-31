ca9849fe09bfb3a1421fba20109f6470ca0aa462
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2024-12-31 10:08:37 +0800

    準備開始大量套用設計模式

--- .firebase/react-blog-fe082/functions/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"YFVJCMNxKNTQx4GpJmbYld0tPWlilFdr3eF0XT86ats=\"}"
--- .firebase/react-blog-fe082/functions/server.js
+++ .firebase/react-blog-fe082/functions/server.js
@@ -1,13 +1,4 @@
 const { onRequest } = require('firebase-functions/v2/https');
-const server = import('firebase-frameworks');
-const functions = require("firebase-functions");
-const next = require("next");
-
-const app = next({ dev: false, conf: { distDir: ".next" } });
-const handle = app.getRequestHandler();
-
-exports.nextApp = functions.https.onRequest((req, res) => {
-  return app.prepare().then(() => handle(req, res));
-}); 
-exports.ssrreactblogfe082 = onRequest({"region":"asia-east1"}, (req, res) => server.then(it => it.handle(req, res)));
+  const server = import('firebase-frameworks');
+  exports.ssrreactblogfe082 = onRequest({"region":"asia-east1"}, (req, res) => server.then(it => it.handle(req, res)));
   
